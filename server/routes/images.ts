f90393947cbdfa0e41d96e4465c628f7ea431e1e
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-11-11 16:55:33 -0700

    nw

--- server/routes/images.ts
+++ server/routes/images.ts
@@ -1,13 +1,42 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as AWS from 'aws-sdk';
-const multer = require('multer');
-const upload = multer({dest: 'uploads/'})
+const multer  = require('multer');
+const fs = require('fs');
+const path = require('path');
+// const upload = multer({ dest: 'uploads/' });
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, path.join(__dirname, '/uploads/'));
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
 
 const router = express.Router();
 
 interface MulterRequest extends express.Request {
   files: any;
+  file: any;
 }
 
 
@@ -16,25 +45,27 @@ AWS.config.update({region: 'us-west-2'});
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 // call S3 to retrieve upload file to specified bucket
 
+router.post('/images', upload.single('xyz'), (request, response: express.Response) => {
 
-router.post('/images', upload.single('image'), (request, response: express.Response) => {
-
-  const file  = (request as unknown as MulterRequest).files;
-  console.log('***REQUEST*** : ', file);
+  // console.log('***REQUEST*** : ', request);
+  const mult  = (request as MulterRequest);
+  console.log('***request*** : ', request);
+  
   // var file = request.files;
   // console.log('***file*** : ', file);
   
   var uploadParams = {Bucket: process.env.AWS_S3_BUCKET, Key: '', Body: ''};
   
   // // Configure the file stream and obtain the upload parameters
-  var fs = require('fs');
-  var fileStream = fs.createReadStream(file.image);
+  var fileStream = fs.createReadStream(mult.files.image.data);
   fileStream.on('error', function(err) {
-    console.log('File Error', err);
+    console.log('XOXOXOXOXOXOXOXOXOX  File Error: ', err);
   });
+  // console.log('***fileStream*** : ', fileStream);
+
+
   uploadParams.Body = fileStream;
-  var path = require('path');
-  uploadParams.Key = path.basename(file);
+  uploadParams.Key = 'hello';
   
   // call S3 to retrieve upload file to specified bucket
   s3.upload (uploadParams, function (err, data) {
