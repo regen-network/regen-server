be5c6d27efa6130e6a45c2c212a2b1c101571ef4
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-11-12 17:06:28 -0700

    request made to s3, access denied

--- server/routes/images.ts
+++ server/routes/images.ts
@@ -1,6 +1,18 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import * as AWS from 'aws-sdk';
+const S3 = require('aws-sdk/clients/s3')
+
+const bucketName = process.env.AWS_S3_BUCKET
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey
+})
+
 const multer  = require('multer');
 const fs = require('fs');
 const path = require('path');
@@ -41,55 +53,40 @@ interface MulterRequest extends express.Request {
   file: any;
 }
 
-
-
-AWS.config.update({region: 'us-west-2'});
-const s3 = new AWS.S3({apiVersion: '2006-03-01'});
-// call S3 to retrieve upload file to specified bucket
-
-router.post('/images', bodyParser.json(), upload.single('image'), (request, response: express.Response) => {
+router.post('/images', bodyParser.json(), (request, response: express.Response) => {
 
   // console.log('***REQUEST*** : ', request.file);
   const mult  = (request as MulterRequest);
-  console.log('***mult*** : ', mult);
-  console.log('***files*** : ', mult.files);
+  // console.log('***files*** : ', mult.files);
 
   const fileStream = Readable.from(mult.files.image.data.toString());
 
-  
-  // var file = mult.files;
-  // console.log('***file*** : ', file);
-  
-  var uploadParams = {Bucket: process.env.AWS_S3_BUCKET, Key: '', Body: ''};
-  // const data = mult.files.image.data;
-  // console.log('***request.file*** : ', request.file);
+  var uploadParams = {Bucket: `${bucketName}/projects/`, Key: '', Body: ''};
 
   // // Configure the file stream and obtain the upload parameters
 
-  // fs.createReadStream(mult.files.image.data).then(s => fileStream = s)
   fileStream.on('error', function(err) {
     console.log('XOXOXOXOXOXOXOXOXOX  File Error: ', err);
   });
-  console.log('***fileStream*** : ', fileStream);
 
 
   uploadParams.Body = fileStream;
-  uploadParams.Key = 'hello';
+  uploadParams.Key = mult.files.image.name;
 
   console.log('***uploadParams*** : ', uploadParams);
 
   
   // call S3 to retrieve upload file to specified bucket
-  s3.upload (uploadParams, function (err, data) {
+  s3.upload(uploadParams, function (err, data) {
     if (err) {
-      console.log("Error", err);
+      console.log("s3 Error", err);
     } if (data) {
       console.log("Upload Success", data.Location);
       response.set('imageUrl', data.Location);
+      response.send({imageUrl: data.Location})
     }
   });
 
-  response.send('okok')
 });
 
 module.exports = router;
