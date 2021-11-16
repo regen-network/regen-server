4f2822d932377fe56cc03d37fe67fb087517c91d
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-11-16 16:38:36 -0700

    cleanup

--- server/routes/images.ts
+++ server/routes/images.ts
@@ -22,25 +22,22 @@ interface MulterRequest extends express.Request {
 }
 
 router.post('/images', bodyParser.json(), (request, response: express.Response) => {
-
-  const mult  = (request as MulterRequest);
-  // console.log('***files*** : ', mult.files);
+  const image  = (request as MulterRequest).files.image;
+  const key  = request.body.filePath;
 
   // // Configure the file stream and obtain the upload parameters
-  const fileStream = Readable.from(mult.files.image.data);
+  const fileStream = Readable.from(image.data);
 
   const uploadParams = {
-    Bucket: `${bucketName}/projects/test`,
-    Key: `${mult.files.image.name}.jpg`,
+    Bucket: `${bucketName}/${key}`,
+    Key: `${image.name}`,
     Body: fileStream
   };
 
   fileStream.on('error', function(err) {
-    console.log('XOXOXOXOXOXOXOXOXOX  File Error: ', err);
+    console.log('File Error: ', err);
   });
 
-  // console.log('***uploadParams*** : ', uploadParams);
-  
   // call S3 to retrieve upload file to specified bucket
   s3.upload(uploadParams, function (err, data) {
     if (err) {
