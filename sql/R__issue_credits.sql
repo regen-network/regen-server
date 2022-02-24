db08f696e2489e746e5764024875905352e7250a
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2022-02-24 13:28:57 -0500

    Add missing type for batch_denom in issue_credits

--- sql/R__issue_credits.sql
+++ sql/R__issue_credits.sql
@@ -31,7 +31,7 @@ create or replace function issue_credits(
   metadata jsonb default '{}',
   initial_issuer_id uuid default uuid_nil(), -- optional party id of the initial issuer in case of 3rd party credits
   reseller_id uuid default uuid_nil(), -- optional wallet id of the reseller, credits gets issued to the reseller in case of 3rd party credits
-  batch_denom default ''
+  batch_denom text default null
 ) returns jsonb as $$
 declare
   v_tokenizer "user";
