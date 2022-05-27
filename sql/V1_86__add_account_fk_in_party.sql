e16f33f517e7b28d13984b68eef04fbb01e838eb
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2022-05-27 18:15:33 -0400

    Adjust party_account_id_fkey to be on delete cascade

--- sql/V1_86__add_account_fk_in_party.sql
+++ sql/V1_86__add_account_fk_in_party.sql
@@ -6,12 +6,14 @@ EXCEPTION
     WHEN duplicate_column THEN
         RAISE NOTICE 'Field already exists. Ignoring...';
     END$$;
-
-DO $$
+    DO
+$$
 BEGIN
     ALTER TABLE party
-        ADD CONSTRAINT party_account_id_fkey FOREIGN KEY (account_id) REFERENCES account (id);
+        ADD CONSTRAINT party_account_id_fkey FOREIGN KEY (account_id) REFERENCES account (id) ON DELETE CASCADE;
+
 EXCEPTION
     WHEN duplicate_object THEN
         RAISE NOTICE 'Constraint already exists. Ignoring...';
+
 END$$;
