0ae3a6aa94746738b8a083f3f755d2d368b2673c
Mark Hagelberg <mark.hagelberg@gmail.com>
2022-06-03 13:48:13 -0600

    index

--- sql/R__get_wallet.sql
+++
@@ -1,3 +0,0 @@
-create or replace function get_wallet_by_address(wallet_address text) returns setof wallet as $$
-  select * from wallet where addr = wallet_address;
-$$ language sql stable;
---
+++ sql/V1_89__add_wallet_id_to_project.sql
@@ -0,0 +1,18 @@
+DO $$
+BEGIN
+    ALTER TABLE project
+        ADD COLUMN wallet_id uuid;
+EXCEPTION
+    WHEN duplicate_column THEN
+        RAISE NOTICE 'Field already exists. Ignoring...';
+    END$$;
+
+DO $$
+BEGIN
+    ALTER TABLE project
+        ADD CONSTRAINT wallet_id_fkey FOREIGN KEY (wallet_id) REFERENCES wallet (id);
+        CREATE INDEX IF NOT EXISTS project_wallet_id_idx ON project (wallet_id);
+EXCEPTION
+    WHEN duplicate_object THEN
+        RAISE NOTICE 'Constraint already exists. Ignoring...';
+END$$;
