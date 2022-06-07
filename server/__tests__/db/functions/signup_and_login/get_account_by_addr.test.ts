5c2b8285a9f115e427c7ee37f61a70d9b89620d8
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2022-06-07 16:40:32 -0400

    Group tests related to signup and login

---
+++ server/__tests__/db/functions/signup_and_login/get_account_by_addr.test.ts
@@ -0,0 +1,28 @@
+import { createAccount, withRootDb } from '../../helpers';
+
+const walletAddr = 'regen123456789';
+
+describe('get_account_by_addr', () => {
+  it('gets the same account for a user with multiple addresses', async () => {
+    await withRootDb(async client => {
+      const accountId = await createAccount(client, walletAddr);
+      const newWalletAddr = 'regenABC123';
+      await client.query(
+        `select * from add_addr_to_account('${accountId}', '${newWalletAddr}')`,
+      );
+      // at this point account_id has two wallets associated to it, so we should
+      // be able to lookup this account with either of these two wallets.
+      const result1 = await client.query(
+        `select account_id from get_account_by_addr('${walletAddr}')`,
+      );
+      const [{ account_id: accountId1 }] = result1.rows;
+      const result2 = await client.query(
+        `select account_id from get_account_by_addr('${walletAddr}')`,
+      );
+      const [{ account_id: accountId2 }] = result2.rows;
+      // make sure that either wallet returns the original accountId
+      expect(accountId).toBe(accountId1);
+      expect(accountId).toBe(accountId2);
+    });
+  });
+});
