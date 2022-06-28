0d607ab414b540cfd85d78ea3564669a73ac3aef
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2022-06-28 15:29:50 -0400

    Clean up migrations

--- sql/V2_14__add_new_party_policies.sql
+++ sql/V2_14__add_new_party_policies.sql
@@ -1,13 +1,3 @@
-DROP POLICY IF EXISTS party_insert_all ON party;
-CREATE POLICY party_insert_all ON party
-    FOR INSERT
-        WITH CHECK (TRUE);
-
-DROP POLICY IF EXISTS party_select_all ON party;
-CREATE POLICY party_select_all ON party
-    FOR SELECT
-        USING (TRUE);
-
 DROP POLICY IF EXISTS party_update_only_by_owner ON party;
 CREATE POLICY party_update_only_by_owner ON party
     FOR UPDATE
