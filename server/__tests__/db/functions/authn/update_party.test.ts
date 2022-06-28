ceb48ba2573ce3524e5e6888801f39c4e5ca9092
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2022-06-28 16:28:49 -0400

    Test the update party policy

---
+++ server/__tests__/db/functions/authn/update_party.test.ts
@@ -0,0 +1,27 @@
+import { createAccount, withRootDb } from '../../helpers';
+
+const walletAddr = 'regen123456789';
+
+describe('update party', () => {
+  test('that a user can update a party that belongs to them', async () => {
+    await withRootDb(async client => {
+      await createAccount(client, walletAddr);
+      await client.query(`set role ${walletAddr}`);
+      const result = await client.query(
+          `update party set name = 'my updated name'`,
+      );
+      expect(result.rowCount).toBe(1);
+    });
+  });
+  test('that a user cannot update another users party', async () => {
+    await withRootDb(async client => {
+      await createAccount(client, walletAddr);
+      await createAccount(client, 'otherUsersAddr');
+      await client.query(`set role ${walletAddr}`);
+      const result = await client.query(
+          `update party set name = 'my updated name'`,
+      );
+      expect(result.rowCount).toBe(1);
+    });
+  });
+});
