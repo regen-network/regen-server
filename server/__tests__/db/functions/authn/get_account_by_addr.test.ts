65ae6af63787132c2c74d7d69a38873409ba0356
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2022-07-20 21:55:28 -0400

    fix: get_account_by_addr returns empty table when an address is not found

--- server/__tests__/db/functions/authn/get_account_by_addr.test.ts
+++ server/__tests__/db/functions/authn/get_account_by_addr.test.ts
@@ -18,13 +18,13 @@ describe('get_account_by_addr', () => {
       // at this point account_id has two wallets associated to it, so we should
       // be able to lookup this account with either of these two wallets.
       const result1 = await client.query(
-        `select account_id from private.get_account_by_addr('${walletAddr}')`,
+        `select id from private.get_account_by_addr('${walletAddr}')`,
       );
-      const [{ account_id: accountId1 }] = result1.rows;
+      const [{ id: accountId1 }] = result1.rows;
       const result2 = await client.query(
-        `select account_id from private.get_account_by_addr('${newWalletAddr}')`,
+        `select id from private.get_account_by_addr('${newWalletAddr}')`,
       );
-      const [{ account_id: accountId2 }] = result2.rows;
+      const [{ id: accountId2 }] = result2.rows;
       // make sure that either wallet returns the original accountId
       expect(accountId).toBe(accountId1);
       expect(accountId).toBe(accountId2);
