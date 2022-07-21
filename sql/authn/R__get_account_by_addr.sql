e6d2b4fa63fc13cf4a8a6bf664ad4f87a8eafc88
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2022-07-20 21:55:28 -0400

    fix: get_account_by_addr returns empty table when an address is not found

--- sql/authn/R__get_account_by_addr.sql
+++ sql/authn/R__get_account_by_addr.sql
@@ -1,11 +1,15 @@
 DROP FUNCTION IF EXISTS private.get_account_by_addr;
-CREATE OR REPLACE FUNCTION private.get_account_by_addr (addr text, OUT account_id uuid)
+CREATE OR REPLACE FUNCTION private.get_account_by_addr (addr text)
+    RETURNS TABLE (
+        id uuid
+    )
 AS $$
 DECLARE
     v_addr text = addr;
 BEGIN
+    RETURN query
     SELECT
-        account.id INTO account_id
+        account.id
     FROM
         wallet
         JOIN party ON party.wallet_id = wallet.id
