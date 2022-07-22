e66b17e81f334bd9753ddd6ccb066b123d0e48b5
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2022-07-22 15:51:41 -0400

    feat: enable graphql to be aware of keplr session

--- server/types.ts
+++ server/types.ts
@@ -2,7 +2,9 @@ import { Request } from 'express';
 import { IncomingMessage } from 'http';
 
 interface User {
+  id?: number;
   sub?: string;
+  address?: string;
 }
 
 export interface UserRequest extends Request {
