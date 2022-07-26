2909b284af9d8f3fa444180d09312926a6d775e1
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2022-07-26 13:13:11 -0400

    chore: move nonce endpoint into web3auth namespace

--- server/Server.ts
+++ server/Server.ts
@@ -24,7 +24,6 @@ import {
   initializePassport,
   InvalidLoginParameter,
 } from './middleware/passport';
-import { PoolClient } from 'pg';
 
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
@@ -196,57 +195,6 @@ app.use(recaptcha);
 app.use(files);
 app.use(metadataGraph);
 app.use('/web3auth', web3auth);
-app.get('/profile', (req: UserIncomingMessage, res) => {
-  if (req.user) {
-    // this is just a dummy api endpoint that illustrates
-    // how we can pull the users info out of `req.user` which
-    // can subsequently be used for fetching data for this user.
-    return res.send(`Hi, ${req.user.address}!`);
-  } else {
-    return res.status(401).send("Sorry, you haven't signed in yet..");
-  }
-});
-app.get('/nonce', async (req, res, next) => {
-  // this endpoint fetches a nonce for a given user by their wallet
-  // address. this is a piece of public information so it is ok to
-  // have this public.
-  if (!req.query.userAddress) {
-    const msg = 'Invalid or missing userAddress query parameter';
-    console.error(msg);
-    const err = new Error(msg);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    err.status = 400;
-    next(err);
-  } else {
-    let client: PoolClient;
-    try {
-      client = await pgPool.connect();
-      const result = await client.query(
-        'select a.nonce from private.get_account_by_addr($1) q join account a on a.id = q.id',
-        [req.query.userAddress],
-      );
-      if (result.rowCount === 0) {
-        const msg = 'User not found for the given address';
-        console.error(msg);
-        const err = new Error(msg);
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        err.status = 404;
-        next(err);
-      } else {
-        const [{ nonce }] = result.rows;
-        return res.status(200).send({ nonce });
-      }
-    } catch (err) {
-      next(err);
-    } finally {
-      if (client) {
-        client.release();
-      }
-    }
-  }
-});
 
 const swaggerOptions = {
   definition: {
