0ab7bbb6a0f349eecf395316a0c17701c8279b13
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2022-07-26 13:13:11 -0400

    chore: move nonce endpoint into web3auth namespace

--- server/routes/web3auth.ts
+++ server/routes/web3auth.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
 import passport from 'passport';
+import { PoolClient } from 'pg';
+import { pgPool } from 'common/pool';
 
 export const web3auth = express.Router();
 
@@ -17,4 +19,46 @@ web3auth.post('/logout', (req, res) => {
   return res.send({
     message: 'You have been logged out!',
   });
+});
+
+web3auth.get('/nonce', async (req, res, next) => {
+  // this endpoint fetches a nonce for a given user by their wallet
+  // address. this is a piece of public information so it is ok to
+  // have this public.
+  if (!req.query.userAddress) {
+    const msg = 'Invalid or missing userAddress query parameter';
+    console.error(msg);
+    const err = new Error(msg);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    err.status = 400;
+    next(err);
+  } else {
+    let client: PoolClient;
+    try {
+      client = await pgPool.connect();
+      const result = await client.query(
+        'select a.nonce from private.get_account_by_addr($1) q join account a on a.id = q.id',
+        [req.query.userAddress],
+      );
+      if (result.rowCount === 0) {
+        const msg = 'User not found for the given address';
+        console.error(msg);
+        const err = new Error(msg);
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        err.status = 404;
+        next(err);
+      } else {
+        const [{ nonce }] = result.rows;
+        return res.status(200).send({ nonce });
+      }
+    } catch (err) {
+      next(err);
+    } finally {
+      if (client) {
+        client.release();
+      }
+    }
+  }
 });
