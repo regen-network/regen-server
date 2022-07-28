5805d4d095cce90f93abc718cc7a6103d7dcf27c
Mark Hagelberg <mark.hagelberg@gmail.com>
2022-07-28 16:59:29 -0600

    chore: IF NOT EXISTS

--- sql/V2_15__add_on_chain_id_to_project.sql
+++ sql/V2_15__add_on_chain_id_to_project.sql
@@ -1,3 +1,3 @@
 ALTER TABLE project
-    ADD COLUMN on_chain_id text unique;
-CREATE INDEX ON project ("on_chain_id");
+    ADD COLUMN IF NOT EXISTS on_chain_id text unique;
+CREATE INDEX IF NOT EXISTS on_chain_id_idx ON project (on_chain_id);
