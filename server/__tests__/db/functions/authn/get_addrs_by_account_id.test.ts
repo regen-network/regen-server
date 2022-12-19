df150af0e289f9fa65113b4e0187e62b21dc20c0
Kyle Lawlor-Bagcal <wgwz@users.noreply.github.com>
2022-12-19 15:09:06 -0500

    feat: make add_addr_to_account private (#230)

--- server/__tests__/db/functions/authn/get_addrs_by_account_id.test.ts
+++ server/__tests__/db/functions/authn/get_addrs_by_account_id.test.ts
@@ -1,4 +1,4 @@
-import { withAuthUserDb } from '../../helpers';
+import { becomeUser, withAuthUserDb } from '../../helpers';
 
 const walletAddr = 'regen123456789';
 
@@ -6,9 +6,15 @@ describe('get_addrs_by_account_id', () => {
   it('returns all addresses associated to a given account id', async () => {
     await withAuthUserDb(walletAddr, async client => {
       const newWalletAddr = 'regenABC123';
-      await client.query(
-        `select * from add_addr_to_account('${newWalletAddr}', 'user')`,
+      const accountIdRes = await client.query(
+        'select * from get_current_account()',
       );
+      const [{ account_id }] = accountIdRes.rows;
+      await becomeUser(client, 'postgres');
+      await client.query(
+        `select * from private.add_addr_to_account('${account_id}', '${newWalletAddr}', 'user')`,
+      );
+      await becomeUser(client, walletAddr);
       // given that we have multiple addrs associated to this particular accountId
       // we should be able to look up all of these addresses.
       const result = await client.query(`select * from get_current_addrs()`);
