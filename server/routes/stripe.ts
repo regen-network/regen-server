11381c8435f5f98a6fac1f39e0f5bee8ff41631b
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2023-01-30 15:25:19 -0500

    style: run and autofix with eslint-plugin-import

--- server/routes/stripe.ts
+++ server/routes/stripe.ts
@@ -7,12 +7,12 @@ import { pgPool } from 'common/pool';
 
 import { runnerPromise } from '../runner';
 
+import Stripe from 'stripe';
+
 let runner;
 runnerPromise.then(res => {
   runner = res;
 });
-
-import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: '2020-08-27',
 });
