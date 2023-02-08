e197436e93b25a047dde497e1bdf5d5c420e13bb
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2023-02-08 17:36:55 -0500

    refactor: improve e2e login/logout tests and utils

--- server/__tests__/utils.ts
+++ server/__tests__/utils.ts
@@ -1,10 +1,16 @@
-import fetch, { Response, Request } from 'node-fetch';
+import fetch, { Response, Request, RequestInfo, Headers } from 'node-fetch';
 import {
+  Bech32Address,
   makeADR36AminoSignDoc,
   serializeSignDoc,
   encodeSecp256k1Signature,
 } from '@keplr-wallet/cosmos';
-import { Hash, PrivKeySecp256k1, PubKeySecp256k1 } from '@keplr-wallet/crypto';
+import {
+  Mnemonic,
+  Hash,
+  PrivKeySecp256k1,
+  PubKeySecp256k1,
+} from '@keplr-wallet/crypto';
 import { genArbitraryData } from '../middleware/keplrStrategy';
 
 export async function fetchCsrf(): Promise<{ cookie: string; token: string }> {
@@ -17,7 +23,7 @@ export async function fetchCsrf(): Promise<{ cookie: string; token: string }> {
 }
 
 export async function CSRFRequest(
-  endpoint: string,
+  endpoint: RequestInfo,
   method: string,
 ): Promise<Request> {
   const { cookie, token } = await fetchCsrf();
@@ -32,13 +38,12 @@ export async function CSRFRequest(
   return request;
 }
 
-export async function performLogin(
+export function genSignature(
   privKey: PrivKeySecp256k1,
   pubKey: PubKeySecp256k1,
   signer: string,
   nonce: string,
-): Promise<Response> {
-  // prepare the data to be signed
+) {
   const data = genArbitraryData(nonce);
   const signDoc = makeADR36AminoSignDoc(signer, data);
   const msg = serializeSignDoc(signDoc);
@@ -48,13 +53,31 @@ export async function performLogin(
     pubKey.toBytes(false),
     signatureBytes,
   );
+  return signature;
+}
+
+interface PerformLogin {
+  response: Response;
+  authHeaders: Headers;
+  csrfHeaders: Headers;
+}
+
+export async function performLogin(
+  privKey: PrivKeySecp256k1,
+  pubKey: PubKeySecp256k1,
+  signer: string,
+  nonce: string,
+): Promise<PerformLogin> {
+  // sign the data
+  const signature = genSignature(privKey, pubKey, signer, nonce);
   // send the request to login API endpoint
   // this step requires retrieving CSRF tokens first
   const req = await CSRFRequest('http://localhost:5000/web3auth/login', 'POST');
-  const resp = await fetch(req, {
+  const response = await fetch(req, {
     body: JSON.stringify({ signature: signature }),
   });
-  return resp;
+  const authHeaders = genAuthHeaders(response.headers, req.headers);
+  return { authHeaders, response, csrfHeaders: req.headers };
 }
 
 export function loginResponseAssertions(resp: Response, signer: string): void {
@@ -88,4 +111,50 @@ export function parseSessionCookies(resp: Response): string {
     })
     .join(';');
   return parsedCookies;
+}
+
+export function genAuthHeaders(
+  loginHeaders: Headers,
+  csrfHeaders: Headers,
+): Headers {
+  // we need to combine the auth cookies, and the csrf cookie
+  const authCookies = loginHeaders.raw()['set-cookie'];
+  const csrfCookies = csrfHeaders.raw()['Cookie'];
+  const cookies = authCookies.concat(csrfCookies);
+  const parsedCookies = cookies
+    .map(entry => {
+      const parts = entry.split(';');
+      const cookiePart = parts[0];
+      return cookiePart;
+    })
+    .join(';');
+  const headers = new Headers([...csrfHeaders.entries()]);
+  headers.delete('cookie');
+  headers.append('cookie', parsedCookies);
+  headers.append('content-type', 'application/json');
+  return headers;
+}
+
+export async function setUpTestAccount(mnemonic: string): Promise<void> {
+  const privKey = new PrivKeySecp256k1(
+    Mnemonic.generateWalletFromMnemonic(mnemonic),
+  );
+  const pubKey = privKey.getPubKey();
+  const signer = new Bech32Address(pubKey.getAddress()).toBech32('regen');
+
+  const resp = await fetch(
+    `http://localhost:5000/web3auth/nonce?userAddress=${signer}`,
+  );
+  // if the nonce was not found then the account does not yet exist
+  if (resp.status === 404) {
+    // create the account if it did not exist
+    const emptyNonce = '';
+    const { response: loginResp } = await performLogin(
+      privKey,
+      pubKey,
+      signer,
+      emptyNonce,
+    );
+    expect(loginResp.status).toBe(200);
+  }
 }
