d3cb1a529174879c3addace32a469bde5c9c86ec
Kyle Lawlor-Bagcal <wgwz@users.noreply.github.com>
2023-05-05 14:37:18 -0400

    refactor!: clean up the database model (#309)
    * refactor!: clean up the database model
    
    * refactor!: migrate org.website to party.website_link and drop org.website
    
    * refactor!: clean up some more columns

--- sql/R__auth.sql
+++ sql/R__auth.sql
@@ -1,25 +1,6 @@
-CREATE OR REPLACE FUNCTION private.create_app_user_if_needed(role text)
-returns void as $$
-begin
-  IF NOT EXISTS (
-      SELECT
-      FROM   pg_catalog.pg_roles
-      WHERE  rolname = role) THEN
-        EXECUTE format('CREATE ROLE %I', role);
-        EXECUTE format('GRANT app_user TO %I', role);
-   END IF;
-end;
-$$ language plpgsql;
-
 CREATE OR REPLACE FUNCTION private.create_auth_user(role text)
 returns void as $$
 begin
   EXECUTE format('CREATE ROLE %I IN ROLE auth_user', role);
 end;
 $$ language plpgsql;
-
--- Test cases:
--- select public.create_app_user_if_needed('test1');
--- set role test1;
--- set role postgres;
--- drop role test1;
