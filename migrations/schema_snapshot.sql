1cc310f9d4184c1ec1425b69f0e5be5d37a693c7
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2023-05-22 15:43:59 -0400

    chore: remove public.flyway_schema_history

--- migrations/schema_snapshot.sql
+++ migrations/schema_snapshot.sql
@@ -696,24 +696,6 @@ CREATE TABLE public.document (
 
 
 --
--- Name: flyway_schema_history; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.flyway_schema_history (
-    installed_rank integer NOT NULL,
-    version character varying(50),
-    description character varying(200) NOT NULL,
-    type character varying(20) NOT NULL,
-    script character varying(1000) NOT NULL,
-    checksum integer,
-    installed_by character varying(100) NOT NULL,
-    installed_on timestamp without time zone DEFAULT now() NOT NULL,
-    execution_time integer NOT NULL,
-    success boolean NOT NULL
-);
-
-
---
 -- Name: metadata_graph; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -882,14 +864,6 @@ ALTER TABLE ONLY public.document
 
 
 --
--- Name: flyway_schema_history flyway_schema_history_pk; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.flyway_schema_history
-    ADD CONSTRAINT flyway_schema_history_pk PRIMARY KEY (installed_rank);
-
-
---
 -- Name: metadata_graph metadata_graph_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -1003,13 +977,6 @@ CREATE INDEX credit_class_registry_id_key ON public.credit_class USING btree (re
 --
 
 CREATE INDEX document_project_id_idx ON public.document USING btree (project_id);
-
-
---
--- Name: flyway_schema_history_s_idx; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX flyway_schema_history_s_idx ON public.flyway_schema_history USING btree (success);
 
 
 --
