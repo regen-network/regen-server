bc7f5130b90fef5551084d30575971f219e0d761
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2023-10-17 21:30:25 -0400

    feat: api endpoints for managing active accounts

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -6,6 +6,7 @@ import {
 } from '../middleware/magicLoginStrategy';
 import { doubleCsrfProtection } from '../middleware/csrf';
 import { GOOGLE_CALLBACK_URL } from '../middleware/googleStrategy';
+import { ensureLoggedIn } from '../middleware/passport';
 
 const router = express.Router();
 
@@ -30,5 +31,36 @@ router.get(
     res.redirect(`${process.env.MARKETPLACE_APP_URL}/profile`);
   },
 );
+
+router.get('/accounts', doubleCsrfProtection, ensureLoggedIn(), (req, res) => {
+  if (!req.session) {
+    return res.sendStatus(500).json({ error: 'req.session is falsy' });
+  }
+  return res.json({
+    activeAccountId: req.session.activeAccountId,
+    activeAccountIds: req.session.activeAccountIds,
+  });
+});
+
+router.post('/accounts', doubleCsrfProtection, ensureLoggedIn(), (req, res) => {
+  const { accountId } = req.query;
+  if (!req.session) {
+    return res.sendStatus(500).json({ error: 'req.session is falsy' });
+  }
+  if (!accountId) {
+    return res.status(400).json({ error: 'missing accountId query parameter' });
+  }
+  if (req.session.activeAccountIds.includes(accountId)) {
+    req.session.activeAccountId = accountId;
+    return res.json({
+      activeAccountId: req.session.activeAccountId,
+      activeAccountIds: req.session.activeAccountIds,
+    });
+  } else {
+    return res
+      .status(401)
+      .json({ error: 'user is not authorized to use the given accountId' });
+  }
+});
 
 export default router;
