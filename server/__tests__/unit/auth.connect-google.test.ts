db7b5ecad157b414703ebe84616b773442d322c7
blushi <marie.gauthier63@gmail.com>
2024-01-30 13:45:42 +0100

    fix: connect google to set main email if not set

--- server/__tests__/unit/auth.connect-google.test.ts
+++ server/__tests__/unit/auth.connect-google.test.ts
@@ -1,8 +1,8 @@
 import { PoolClient } from 'pg';
 
 import { connectGoogleAccount } from '../../middleware/connectGoogleStrategy';
-import { withRootDb } from '../db/helpers';
-import { createWeb2Account } from '../utils';
+import { createAccountWithAuthUser, withRootDb } from '../db/helpers';
+import { createWeb2Account, genRandomRegenAddress } from '../utils';
 import { UnauthorizedError } from '../../errors';
 
 const email = 'john@doe.com';
@@ -29,6 +29,51 @@ describe('auth connect google strategy', () => {
       expect(privateAccountQuery.rows[0].email).toEqual(email);
       expect(privateAccountQuery.rows[0].google_email).toEqual(googleEmail);
       expect(privateAccountQuery.rows[0].google).toEqual(googleId);
+    });
+  });
+  test('when a logged-in user with no email connects to google, it should update the account email, google id and google email', async () => {
+    await withRootDb(async (client: PoolClient) => {
+      const walletAddr = genRandomRegenAddress();
+      const { accountId } = await createAccountWithAuthUser(client, walletAddr);
+
+      await connectGoogleAccount({
+        email: googleEmail,
+        verified: 'true',
+        googleId,
+        accountId,
+        client,
+      });
+      const privateAccountQuery = await client.query(
+        'SELECT * FROM private.account WHERE id = $1',
+        [accountId],
+      );
+      expect(privateAccountQuery.rowCount).toBe(1);
+      expect(privateAccountQuery.rows[0].email).toEqual(googleEmail);
+      expect(privateAccountQuery.rows[0].google_email).toEqual(googleEmail);
+      expect(privateAccountQuery.rows[0].google).toEqual(googleId);
+    });
+  });
+  test('when a logged-in user with no email connects to google, it should throw an error if google_email is already used as main email by another account', async () => {
+    await withRootDb(async (client: PoolClient) => {
+      // Create account with googleEmail as main email
+      await createWeb2Account({ client, email: googleEmail });
+
+      // Create another account with no email
+      const walletAddr = genRandomRegenAddress();
+      const { accountId } = await createAccountWithAuthUser(client, walletAddr);
+
+      // Try to connect it to google using googleEmail
+      expect(
+        connectGoogleAccount({
+          email: googleEmail,
+          verified: 'true',
+          googleId,
+          accountId,
+          client,
+        }),
+      ).rejects.toThrow(
+        'duplicate key value violates unique constraint "account_email_key"',
+      );
     });
   });
   test('when a logged-in user connects to google, it should throw an error if google_email is already used', async () => {
